23a71dd299964d495d19d1b5e27017298d399c78
ksugaevskaya <ksu.gaevskaya@gmail.com>
2026-04-05 23:52:57 +0200

    feat: add service logic to create comment

--- src/comments/comments.service.ts
+++ src/comments/comments.service.ts
@@ -1,11 +1,24 @@
 import { Injectable } from '@nestjs/common';
 import { CreateCommentDto } from './dto/create-comment.dto';
 import { UpdateCommentDto } from './dto/update-comment.dto';
+import { randomUUID } from 'crypto';
+import { CommentType } from './entities/comment.entity';
 
 @Injectable()
 export class CommentsService {
+  private comments: CommentType[] = [];
+
   create(createCommentDto: CreateCommentDto) {
-    return 'This action adds a new comment';
+    const newComment = {
+      id: randomUUID(),
+      content: createCommentDto.content,
+      articleId: createCommentDto.articleId,
+      authorId: createCommentDto.authorId,
+      createdAt: Date.now(),
+    };
+
+    this.comments.push(newComment);
+    return newComment;
   }
 
   findAll() {
