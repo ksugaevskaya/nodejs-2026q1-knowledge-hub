2d6f585c2089d48f75cd2a15b159ae43d27bd25b
ksugaevskaya <ksu.gaevskaya@gmail.com>
2026-04-06 00:45:08 +0200

    feat: add swagger documentation

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -12,6 +13,16 @@ async function bootstrap() {
       transform: true,
     }),
   );
+
+  const config = new DocumentBuilder()
+    .setTitle('Knowledge Hub')
+    .setDescription(
+      'Knowledge hub service for managing articles, categories, and comments',
+    )
+    .setVersion('1.0.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('doc', app, document);
 
   await app.listen(4000);
 }
