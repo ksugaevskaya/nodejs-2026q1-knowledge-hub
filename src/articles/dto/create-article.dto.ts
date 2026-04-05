6fcba0b7dba6e72665824b411e5c1d6b75cc41f3
ksugaevskaya <ksu.gaevskaya@gmail.com>
2026-04-05 21:52:45 +0200

    feat: CreateArticleDto

--- src/articles/dto/create-article.dto.ts
+++ src/articles/dto/create-article.dto.ts
@@ -1,1 +1,22 @@
-export class CreateArticleDto {}
+import { IsString, IsOptional, IsUUID, IsArray } from 'class-validator';
+
+export class CreateArticleDto {
+  @IsString()
+  title: string;
+
+  @IsString()
+  content: string;
+
+  @IsOptional()
+  @IsUUID()
+  authorId?: string;
+
+  @IsOptional()
+  @IsUUID()
+  categoryId?: string;
+
+  @IsOptional()
+  @IsArray()
+  @IsString({ each: true })
+  tags?: string[];
+}
