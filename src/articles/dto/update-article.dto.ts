4661f2a96bbdf8a59c18d0affae02448ac6b07f4
ksugaevskaya <ksu.gaevskaya@gmail.com>
2026-04-05 23:03:27 +0200

    feat: add UpdateArticleDto

--- src/articles/dto/update-article.dto.ts
+++ src/articles/dto/update-article.dto.ts
@@ -1,4 +1,28 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateArticleDto } from './create-article.dto';
+import { IsOptional, IsString, IsUUID, IsArray, IsEnum } from 'class-validator';
+import { ArticleStatus } from '../entities/article.entity';
 
-export class UpdateArticleDto extends PartialType(CreateArticleDto) {}
+export class UpdateArticleDto {
+  @IsOptional()
+  @IsString()
+  title?: string;
+
+  @IsOptional()
+  @IsString()
+  content?: string;
+
+  @IsOptional()
+  @IsEnum(ArticleStatus)
+  status?: ArticleStatus;
+
+  @IsOptional()
+  @IsUUID('4')
+  categoryId?: string | null;
+
+  @IsOptional()
+  @IsUUID('4')
+  authorId?: string | null;
+
+  @IsOptional()
+  @IsArray()
+  tags?: string[];
+}
