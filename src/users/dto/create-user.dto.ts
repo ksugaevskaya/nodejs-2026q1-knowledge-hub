d595daec461d8a58c25db5285b7cb3b5003c4e86
ksugaevskaya <ksu.gaevskaya@gmail.com>
2026-04-05 19:26:44 +0200

    feat: add CreateUserDto

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,14 @@
-export class CreateUserDto {}
+import { IsString, IsEnum, MinLength } from 'class-validator';
+import { UserRole } from '../entities/user.entity';
+
+export class CreateUserDto {
+  @IsString()
+  login: string;
+
+  @IsString()
+  @MinLength(6)
+  password: string;
+
+  @IsEnum(UserRole)
+  role?: UserRole;
+}
