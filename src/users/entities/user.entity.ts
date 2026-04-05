a610927f7f6dade3e2a6c57a8de525e6534410fe
ksugaevskaya <ksu.gaevskaya@gmail.com>
2026-04-05 18:48:08 +0200

    feat: add user entity

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,1 +1,14 @@
-export class User {}
+export enum UserRole {
+  ADMIN = 'admin',
+  EDITOR = 'editor',
+  VIEWER = 'viewer',
+}
+
+export interface User {
+  id: string;
+  login: string;
+  password: string;
+  role: UserRole;
+  createdAt: number;
+  updatedAt: number;
+}
