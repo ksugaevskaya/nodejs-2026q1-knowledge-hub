3aa6137bcd0e9d811da48b83de71977d1edceffd
ksugaevskaya <ksu.gaevskaya@gmail.com>
2026-04-05 23:46:40 +0200

    feat: CreateCommentDto

--- src/comments/dto/create-comment.dto.ts
+++ src/comments/dto/create-comment.dto.ts
@@ -1,1 +1,14 @@
-export class CreateCommentDto {}
+import { IsString, IsNotEmpty, IsUUID, IsOptional } from 'class-validator';
+
+export class CreateCommentDto {
+  @IsString()
+  @IsNotEmpty()
+  content: string;
+
+  @IsUUID('4')
+  articleId: string;
+
+  @IsOptional()
+  @IsUUID('4')
+  authorId?: string | null;
+}
