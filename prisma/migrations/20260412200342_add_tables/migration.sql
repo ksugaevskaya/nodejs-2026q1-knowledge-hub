94ce3d209a6223e8d7f647c73c65a4a529841840
ksugaevskaya <ksu.gaevskaya@gmail.com>
2026-04-12 22:04:01 +0200

    fix: lowercase enum in migration

--- prisma/migrations/20260412200342_add_tables/migration.sql
+++ prisma/migrations/20260412200342_add_tables/migration.sql
@@ -1,15 +1,15 @@
 -- CreateEnum
-CREATE TYPE "Role" AS ENUM ('ADMIN', 'EDITOR', 'VIEWER');
+CREATE TYPE "Role" AS ENUM ('admin', 'editor', 'viewer');
 
 -- CreateEnum
-CREATE TYPE "ArticleStatus" AS ENUM ('DRAFT', 'PUBLISHED', 'ARCHIVED');
+CREATE TYPE "ArticleStatus" AS ENUM ('draft', 'published', 'archived');
 
 -- CreateTable
 CREATE TABLE "User" (
     "id" UUID NOT NULL,
     "login" TEXT NOT NULL,
     "password" TEXT NOT NULL,
-    "role" "Role" NOT NULL DEFAULT 'VIEWER',
+    "role" "Role" NOT NULL DEFAULT 'viewer',
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
 
@@ -21,7 +21,7 @@ CREATE TABLE "Article" (
     "id" UUID NOT NULL,
     "title" TEXT NOT NULL,
     "content" TEXT NOT NULL,
-    "status" "ArticleStatus" NOT NULL DEFAULT 'DRAFT',
+    "status" "ArticleStatus" NOT NULL DEFAULT 'draft',
     "authorId" UUID,
     "categoryId" UUID,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
