5890a69c442b9b95008dad79025d26d9927f4ed6
ksugaevskaya <ksu.gaevskaya@gmail.com>
2026-04-12 23:44:01 +0200

    feat: add transaction for user removal

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -155,8 +155,17 @@ export class UsersService {
       throw new NotFoundException('User not found');
     }
 
-    await this.prisma.user.delete({
-      where: { id },
-    });
+    await this.prisma.$transaction([
+      this.prisma.article.updateMany({
+        where: { authorId: id },
+        data: { authorId: null },
+      }),
+      this.prisma.comment.deleteMany({
+        where: { authorId: id },
+      }),
+      this.prisma.user.delete({
+        where: { id },
+      }),
+    ]);
   }
 }
