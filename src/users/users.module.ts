4447ae0671ad631132187fd3b45994833bd3bcc3
ksugaevskaya <ksu.gaevskaya@gmail.com>
2026-04-12 22:21:48 +0200

    feat: migrate users service to use prisma service

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,13 +1,11 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
-import { ArticlesModule } from '../articles/articles.module';
-import { CommentsModule } from '../comments/comments.module';
+import { PrismaService } from '../prisma/prisma.service';
 
 @Module({
-  imports: [ArticlesModule, CommentsModule],
   controllers: [UsersController],
-  providers: [UsersService],
+  providers: [UsersService, PrismaService],
   exports: [UsersService],
 })
 export class UsersModule {}
