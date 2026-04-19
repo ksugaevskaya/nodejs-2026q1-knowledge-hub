df277df734debf2577da23166cc0d7344608d6d5
ksugaevskaya <ksu.gaevskaya@gmail.com>
2026-04-19 21:53:01 +0200

    feat: add auth controller & service

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
-import { CreateAuthDto } from './dto/create-auth.dto';
-import { UpdateAuthDto } from './dto/update-auth.dto';
+import { CreateAuthDto } from './dto/signup';
+import { UpdateAuthDto } from './dto/login';
 
 @Injectable()
 export class AuthService {
