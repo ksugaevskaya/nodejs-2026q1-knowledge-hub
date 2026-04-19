ccc9f6c86101d39fdb7ea419031fa3802bc1dd85
ksugaevskaya <ksu.gaevskaya@gmail.com>
2026-04-19 22:19:55 +0200

    feat: add POST endpoint to sign up new user

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,42 +1,13 @@
-import {
-  Controller,
-  Get,
-  Post,
-  Body,
-  Patch,
-  Param,
-  Delete,
-} from '@nestjs/common';
+import { Controller, Post, Body } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { CreateAuthDto } from './dto/signup';
-import { UpdateAuthDto } from './dto/login';
+import { SignupDto } from './dto/signup';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
-  @Post()
-  create(@Body() createAuthDto: CreateAuthDto) {
-    return this.authService.create(createAuthDto);
-  }
-
-  @Get()
-  findAll() {
-    return this.authService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.authService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateAuthDto: UpdateAuthDto) {
-    return this.authService.update(+id, updateAuthDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.authService.remove(+id);
+  @Post('signup')
+  signUp(@Body() SignupDto: SignupDto) {
+    return this.authService.signUp(SignupDto);
   }
 }
