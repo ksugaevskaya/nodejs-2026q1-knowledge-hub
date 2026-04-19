7c1313086ba70918bcff9c12dc89a39132ca1e6c
ksugaevskaya <ksu.gaevskaya@gmail.com>
2026-04-19 23:26:54 +0200

    feat: make endpoints public

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -3,23 +3,27 @@ import { AuthService } from './auth.service';
 import { SignupDto } from './dto/signup';
 import { LoginDto } from './dto/login';
 import { RefreshDto } from './dto/refresh';
+import { Public } from './public-decorator';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
+  @Public()
   @Post('signup')
   @HttpCode(HttpStatus.CREATED)
   signUp(@Body() data: SignupDto) {
     return this.authService.signUp(data);
   }
 
+  @Public()
   @Post('login')
   @HttpCode(HttpStatus.OK)
   login(@Body() data: LoginDto) {
     return this.authService.login(data);
   }
 
+  @Public()
   @Post('refresh')
   @HttpCode(HttpStatus.OK)
   refresh(@Body() data: RefreshDto) {
