21b8884b35223942d25ef137adfabb58547a515d
ksugaevskaya <ksu.gaevskaya@gmail.com>
2026-05-03 21:19:31 +0300

    feat: add article module to ai module

--- src/ai/ai.module.ts
+++ src/ai/ai.module.ts
@@ -5,8 +5,10 @@ import { GeminiService } from './gemini.service';
 import { AiCacheService } from './internal/ai-cache.service';
 import { AiRateLimitService } from './internal/ai-rate-limit.service';
 import { AiUsageTrackerService } from './internal/ai-usage-tracker.service';
+import { ArticlesModule } from 'src/articles/articles.module';
 
 @Module({
+  imports: [ArticlesModule],
   controllers: [AiController],
   providers: [
     AiService,
